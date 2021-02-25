4f3ccf1f814d94adfcd1155615385b1c354e6644
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-02-25 16:18:15 -0300

    update

--- Aula01/prog1/Olamundo.java
+++ Aula01/prog1/Olamundo.java
@@ -2,7 +2,6 @@ class Olamundo{
     public static void main(String[] args) {
         //Comenta linha
         System.out.println("Hello World!");
-        System.out.println("asl;dkjfha;shf");
         /*comenta entre essas coisas*/
     }
 }
