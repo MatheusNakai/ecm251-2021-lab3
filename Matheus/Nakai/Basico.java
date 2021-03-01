f64fbef5efb05ce0e933b660eb34d78e230f3adb
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-03-01 12:24:19 -0300

    Create Basico.java

---
+++ Matheus/Nakai/Basico.java
@@ -0,0 +1,23 @@
+package Matheus.Nakai;
+
+public class Basico {
+    public static void main(String[] args) {
+        int numero;
+        float hp;
+        double atk;
+        boolean gigantamax;
+        String nome;
+
+        //Atribuicao para as variaveis
+        numero=25;
+        hp=155.0f;
+        atk=255.0;
+        gigantamax=true;
+        nome="Pikachu";
+
+        //exibe os valores das variaveis
+        System.out.println("Nome:" + nome);
+        System.out.println("Gigantamax: "+ gigantamax);
+        System.out.println("Hp: " +hp+ "\n" + "atk: "+ atk);
+    }
+}
