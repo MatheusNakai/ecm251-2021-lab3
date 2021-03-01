a4dc878dce48e0076b62f8956de81f8fbf0d81e4
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-03-01 12:52:20 -0300

    patch 1.1.3

--- Matheus/Nakai/Basico2.java
+++ Matheus/Nakai/Basico2.java
@@ -5,11 +5,32 @@ public class Basico2 {
         int ano= 2021;
         
         //estrutura de decisao simples
-        if(ano%2==0){
-            System.out.println("Ano par!");
-        }
-        if(ano%2==1){
-            System.out.println("Ano impar!");
+//        if(ano%2==0){
+//            System.out.println("Ano par!");
+//        }
+//        if(ano%2==1){
+//            System.out.println("Ano impar!");
+//        }
+
+        //Estrutura de decisao composta
+//        if(ano%2==0){
+//            System.out.println("Ano par!");
+//        }else{
+//            System.out.println("Ano Impar!");
+//        }
+
+        //varias decisoes
+        switch (ano){
+            case 2020:
+                System.out.println("Foda!");
+                break;
+            case 1996:
+                System.out.println("Pokemon Red!!");
+                break;
+            default :
+                System.out.println("Quase Boku no hero saindo!");
+                break;
+    
         }
     }
 }
