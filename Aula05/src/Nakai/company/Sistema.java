511bcc8aaaf8c07b55dcec031f7d206e252d537f
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-03-15 12:13:30 -0300

    Criar Loop

--- Aula05/src/Nakai/company/Sistema.java
+++ Aula05/src/Nakai/company/Sistema.java
@@ -1,7 +1,44 @@
 package Nakai.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Sistema {
+    private boolean continuarExecucao;
+    private Scanner scanner;
+
+    public Sistema() {
+        this.continuarExecucao=true;
+        this.scanner = new Scanner(System.in);
+    }
+
     public void executar() {
-        System.out.println("Aqui!");
+
+        while(continuarExecucao){
+            exibirMenu();
+            int opcao = scanner.nextInt();
+            avaliarOpcao(opcao);
+        }
+    }
+
+    private void avaliarOpcao(int opcao) {
+        switch (opcao){
+            case 0:
+                System.out.println("Obrigado por ter utilizado nosso sistem!" );
+                this.continuarExecucao=false;
+                break;
+            default:
+                System.out.println("Funcionalidade ainda nao implementada");
+        }
+    }
+
+    private void exibirMenu() {
+        System.out.println("Bem-vindo ao MauaBank");
+        System.out.println("1 - Visualizar Saldo");
+        System.out.println("2 - Depositar Dinheiro");
+        System.out.println("3 - Sacar Dinheiro");
+        System.out.println("4 - Transferir Dinheiro");
+        System.out.println("5 - Pagar Titulo");
+        System.out.println("0 - Sair do Sistema");
     }
 }
