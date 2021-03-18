bb97141772b61bb45514f6bd42784b4d88e86646
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-03-18 09:12:06 -0300

    Validador CPF

---
+++ Aula05-teoria/src/Nakai/company/Main.java
@@ -0,0 +1,10 @@
+package Nakai.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String cpf="529.982.247-25";
+        System.out.println("Cpf valido:"+ ValidadorCPF.validarCPF(cpf));
+    }
+}
