5fa910a5e3302893edb4c4ab4b4eb32956368335
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-03-28 16:53:52 -0300

    criacao de construtor Conta

--- Atividade1/src/Nakai/company/Conta.java
+++ Atividade1/src/Nakai/company/Conta.java
@@ -3,6 +3,12 @@ package Nakai.company;
 public class Conta {
     private int numero;
     private double saldo;
+    private Usuario usuario;
 
+    public Conta(int numero, double saldo, Usuario usuario) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.usuario = usuario;
+    }
 
 }
