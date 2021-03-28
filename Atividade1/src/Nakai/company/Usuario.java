e9cc914ae4eb02576c1c25397265bec1ae3243b1
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-03-28 16:42:54 -0300

    Criacao da classe usuario com construtor

---
+++ Atividade1/src/Nakai/company/Usuario.java
@@ -0,0 +1,11 @@
+package Nakai.company;
+
+public class Usuario {
+    private String usuario;
+    private String cpf;
+
+    public Usuario(String usuario, String cpf) {
+        this.usuario = usuario;
+        this.cpf = cpf;
+    }
+}
