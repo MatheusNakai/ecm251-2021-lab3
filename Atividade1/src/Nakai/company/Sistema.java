6792e9c3e976f881758cff122ec6735be661b983
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-03-28 17:12:20 -0300

    metodo sistema

--- Atividade1/src/Nakai/company/Sistema.java
+++ Atividade1/src/Nakai/company/Sistema.java
@@ -5,5 +5,11 @@ import java.util.Scanner;
 public class Sistema {
     private boolean continuarExecucao = true;
     private Scanner scanner;
+    private Conta conta;
+    public Sistema(){
+        this.continuarExecucao=true;
+        this.scanner = new Scanner(System.in);
+    }
+
 
 }
