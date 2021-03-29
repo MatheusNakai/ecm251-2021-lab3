7022e759e6da05f25f6103a92b3a4fd90f4a6c1e
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-03-29 15:09:15 -0300

    ajuste da classe usuario

--- Atividade1/src/Nakai/company/Conta.java
+++ Atividade1/src/Nakai/company/Conta.java
@@ -5,6 +5,7 @@ public class Conta {
     private double saldo;
     private Usuario usuario;
 
+
     public Conta(int numero, double saldo, Usuario usuario) {
         this.numero = numero;
         this.saldo = saldo;
@@ -30,4 +31,5 @@ public class Conta {
         }
         return false;
     }
+
 }
--- Atividade1/src/Nakai/company/Usuario.java
+++ Atividade1/src/Nakai/company/Usuario.java
@@ -3,9 +3,13 @@ package Nakai.company;
 public class Usuario {
     private String usuario;
     private String cpf;
+    private String senha;
+    private String email;
 
-    public Usuario(String usuario, String cpf) {
+    public Usuario(String usuario, String cpf, String senha, String email) {
         this.usuario = usuario;
         this.cpf = cpf;
+        this.senha = senha;
+        this.email = email;
     }
 }
