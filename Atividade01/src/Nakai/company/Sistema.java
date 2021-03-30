b28755dcb25cbb0a5002865d4b57cce8486a2bc2
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-03-30 14:55:33 -0300

    Metodo que mostrar menu

--- Atividade01/src/Nakai/company/Sistema.java
+++ Atividade01/src/Nakai/company/Sistema.java
@@ -7,4 +7,15 @@ public class Sistema {
     public static int LogedMenu=0;
     public static Usuario usuarioLogado=null;
 
+    public static void run(){
+        System.out.println("");
+    }
+
+    public static void mostrarMainMenu(Scanner scanner){
+        System.out.println("Por favor, escolha uma opção:");
+        System.out.println("1 - Criar Usuario.");
+        System.out.println("2 - Login como usuário.");
+        System.out.println("3 - Terminar programa.");
+        
+    }
 }
