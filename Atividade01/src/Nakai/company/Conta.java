8471db0b2611075b1bedd6737b9917a3c92a0a64
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-03-30 15:52:13 -0300

    criacao do contrutor de conta

--- Atividade01/src/Nakai/company/Conta.java
+++ Atividade01/src/Nakai/company/Conta.java
@@ -9,7 +9,14 @@ public class Conta {
     private static ArrayList<Conta> contas = new ArrayList<>(); //Armazena Contas criadas
     private Usuario Dono;
 
-
+    public Conta(int idConta, double saldo, Usuario dono) {
+        this.idConta = idConta;
+        this.saldo = saldo;
+        Dono = dono;
+        Dono.atribuirCOnta(this);
+        controlador++;
+        System.out.println("Nova Conta ("+idConta+") de "+dono.nomeInteiro+"criada com sucesso.");
+    }
 
     private void depositar(double valor){
         this.saldo+=valor;
