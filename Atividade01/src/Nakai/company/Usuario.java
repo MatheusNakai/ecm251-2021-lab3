d126a1f0b256dc7bddc0648b3bb26d1a5efa9dd6
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-03-30 11:01:23 -0300

    Criacao da classe usuario
    Com verificacao de usuarios ja existentes

---
+++ Atividade01/src/Nakai/company/Usuario.java
@@ -0,0 +1,23 @@
+package Nakai.company;
+
+import java.util.ArrayList;
+
+public class Usuario {
+    private static ArrayList<Usuario> usuarios= new ArrayList<>(); //armazena usuarios criados
+    public String nomeInteiro;
+    public String email;
+    private String senha;
+
+
+
+    public boolean UsuarioExistente(String nome){ //auxilio de Bruno Villardi
+        for (Usuario i : Usuario.usuarios){
+            if(i.nomeInteiro.equals(nome)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
