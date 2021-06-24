8f1c1d7395218849c142f9c87ccce1a2d01877e4
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-24 20:17:04 -0300

    Adicao de atributos na classe Membro

---
+++ Atividade_02/src/br/maua/membros/Membro.java
@@ -0,0 +1,8 @@
+package br.maua.membros;
+
+public abstract class Membro {
+    protected String nickname;
+    protected String senha;
+    protected int id;
+
+}
--- Atividade_02/src/br/maua/models/Membro.java
+++
@@ -1,4 +0,0 @@
-package br.maua.models;
-
-public abstract class Membro {
-}
