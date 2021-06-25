b164246e51f6e538afaf0089028a318967972d03
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 16:50:06 -0300

    Implementacao do metodo para criar novos Memebros

--- Atividade_02/src/br/maua/controlador/Sistema.java
+++ Atividade_02/src/br/maua/controlador/Sistema.java
@@ -1,7 +1,7 @@
 package br.maua.controlador;
 
 import br.maua.enums.Funcao;
-import br.maua.membros.Membro;
+import br.maua.membros.*;
 
 import java.util.Scanner;
 
@@ -36,17 +36,44 @@ public class Sistema {
     }
 
     private Membro criarMemebro(){
-     System.out.println("Criacao de Membros:\n"+"");
+        System.out.println("Criacao de Membros:\n"+"");
 
-     System.out.println("Digite um nickname que deseje utilizar:");
-     String nickname = scanner.next();
+        System.out.println("Digite um nickname que deseje utilizar:");
+        String nickname = scanner.next();
 
-     System.out.println("Digite a senha:");
-     String senha = scanner.next();
+        System.out.println("Digite a senha:");
+        String senha = scanner.next();
+
+        System.out.println("Digite a Funcao do novo membro:\n(1 - Mobile Member), (2 - Big Brother), (3 - Heavy Lifter), (4 - Script Guy)");
+        String role = scanner.next();
+        Funcao funcao = tipodeMembro(role);
+        
+        if(funcao ==null){
+            return null;
+        }
+        Membro membro;
+        switch (funcao){
+            case BIG_BROTHER:
+                membro = new Big_Brothers(nickname,senha,funcao);
+                break;
+                
+            case HEAVY_LIFTER:
+                membro = new Heavy_Lifters(nickname,senha,funcao);
+                break;
+            
+            case SCRIPT_GUY:
+                membro = new Script_Guys(nickname,senha,funcao);
+                break;
+                
+            case MOBILE_MEMBER:
+                membro = new Mobile_Members(nickname,senha,funcao);
+                break;
 
 
-
-
+            default:
+                throw new IllegalStateException("Unexpected value: " + funcao);
+        }
+        return membro;
 
  }
 }
