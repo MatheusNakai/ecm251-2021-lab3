1330a467e9e0cff5c3700203698280f05b4db47c
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 18:32:30 -0300

    Implementacao dos metodos apresentar e postarMensagem no Big_Brothers

--- Atividade_02/src/br/maua/membros/Big_Brothers.java
+++ Atividade_02/src/br/maua/membros/Big_Brothers.java
@@ -1,6 +1,7 @@
 package br.maua.membros;
 
 import br.maua.enums.Funcao;
+import br.maua.enums.Horario;
 
 public class Big_Brothers extends Membro{
     private String assinaturaRegular = "Sempre ajudando as pessoas membros ou não S2!";
@@ -9,5 +10,18 @@ public class Big_Brothers extends Membro{
 
     public Big_Brothers(String nickname, String senha, Funcao funcao) {
         super(nickname, senha, funcao);
+    }
+
+    @Override
+    public String apresentar() {
+        return "Ola eu sou o "+nickname+" da MAsK_S0c13ty, sou um dos Big Brothers.";
+    }
+
+    @Override
+    public String postarMensagem(Horario horario) {
+        if(horario == Horario.REGULAR){
+            return assinaturaRegular;
+        }
+        else{return assinaturaExtra;}
     }
 }
