62fd92173cb246e932d7a6fcaada78e13689120d
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 13:50:57 -0300

    Implementacao do contrutor da classe Big_Brothers com super

--- Atividade_02/src/br/maua/membros/Big_Brothers.java
+++ Atividade_02/src/br/maua/membros/Big_Brothers.java
@@ -1,7 +1,13 @@
 package br.maua.membros;
 
+import br.maua.enums.Funcao;
+
 public class Big_Brothers extends Membro{
     private String assinaturaRegular = "Sempre ajudando as pessoas membros ou não S2!";
     private String assinaturaExtra = "...";
 
+
+    public Big_Brothers(String nickname, String senha, Funcao funcao) {
+        super(nickname, senha, funcao);
+    }
 }
