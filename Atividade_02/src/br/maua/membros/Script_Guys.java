93571c4e5c1df309b3f841151bd689f15babb9f2
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 14:00:20 -0300

    Adicao dos atributos das assinaturas regular e extra da classe Script_Guys

--- Atividade_02/src/br/maua/membros/Script_Guys.java
+++ Atividade_02/src/br/maua/membros/Script_Guys.java
@@ -3,7 +3,8 @@ package br.maua.membros;
 import br.maua.enums.Funcao;
 
 public class Script_Guys extends Membro{
-
+    private String assinaturaRegular = "Prazer em ajudar novos amigos de código!";
+    private String assinaturaExtra = "QU3Ro_S3us_r3curs0s.py";
 
 
     public Script_Guys(String nickname, String senha, Funcao funcao) {
