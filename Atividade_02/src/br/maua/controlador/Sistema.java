535547ee9ba26379a76eb7ed0ea81c46539f5d75
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 16:16:59 -0300

    Criacao do metodo criarMembro

--- Atividade_02/src/br/maua/controlador/Sistema.java
+++ Atividade_02/src/br/maua/controlador/Sistema.java
@@ -1,11 +1,25 @@
 package br.maua.controlador;
 
+import br.maua.membros.Membro;
+
+import java.util.Scanner;
+
 public class Sistema {
+    private Scanner scanner= new Scanner(System.in);
+
+
+
+ private Membro criarMemebro(){
+     System.out.println("Criacao de Membros:\n"+"");
+
+     System.out.println("Digite um nickname que deseje utilizar:");
+     String nickname = scanner.next();
+
+     System.out.println("Digite a senha:");
+     String senha = scanner.next();
 
 
 
 
-    public static void exibirMenu(){
-        System.out.println("1 - Login\n2 - Registrar um membro\n3 - ");
-    }
+ }
 }
