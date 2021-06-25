1830aa1c2ab0262d829344fa798bc74a3aed2fe6
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 18:34:14 -0300

    Implementacao dos metodos apresentar e postarMensagem no Script_Guys

--- Atividade_02/src/br/maua/membros/Script_Guys.java
+++ Atividade_02/src/br/maua/membros/Script_Guys.java
@@ -1,6 +1,7 @@
 package br.maua.membros;
 
 import br.maua.enums.Funcao;
+import br.maua.enums.Horario;
 
 public class Script_Guys extends Membro{
     private String assinaturaRegular = "Prazer em ajudar novos amigos de código!";
@@ -9,5 +10,18 @@ public class Script_Guys extends Membro{
 
     public Script_Guys(String nickname, String senha, Funcao funcao) {
         super(nickname, senha, funcao);
+    }
+
+    @Override
+    public String apresentar() {
+        return "Ola eu sou o "+nickname+" da MAsK_S0c13ty, sou um dos Script Guys.";
+    }
+
+    @Override
+    public String postarMensagem(Horario horario) {
+        if(horario == Horario.REGULAR){
+            return assinaturaRegular;
+        }
+        else{return assinaturaExtra;}
     }
 }
