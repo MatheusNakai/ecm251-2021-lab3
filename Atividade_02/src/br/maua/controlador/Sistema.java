670045a3d10deaea5a54b2e74048f26ad1608cb0
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 16:36:24 -0300

    Criacao do metodo para que o usuario possa escolher o enum da funcao

--- Atividade_02/src/br/maua/controlador/Sistema.java
+++ Atividade_02/src/br/maua/controlador/Sistema.java
@@ -1,5 +1,6 @@
 package br.maua.controlador;
 
+import br.maua.enums.Funcao;
 import br.maua.membros.Membro;
 
 import java.util.Scanner;
@@ -7,9 +8,34 @@ import java.util.Scanner;
 public class Sistema {
     private Scanner scanner= new Scanner(System.in);
 
+    /**
+     *
+     * @param funcao: String para o auxilio de definir a funcao do funcionario que ira ser criado
+     * @return Um tipo de Funcao dentro do Enum Funcao
+     */
+    private Funcao tipodeMembro(String funcao){
+        switch (funcao){
+            case "1":
+                return Funcao.MOBILE_MEMBER;
+
+            case "2":
+                return Funcao.BIG_BROTHER;
 
 
- private Membro criarMemebro(){
+            case "3":
+                return Funcao.HEAVY_LIFTER;
+
+
+            case "4":
+                return Funcao.SCRIPT_GUY;
+
+
+            default:
+                throw new IllegalStateException("Valor Invalido: "+funcao);
+        }
+    }
+
+    private Membro criarMemebro(){
      System.out.println("Criacao de Membros:\n"+"");
 
      System.out.println("Digite um nickname que deseje utilizar:");
@@ -17,6 +43,7 @@ public class Sistema {
 
      System.out.println("Digite a senha:");
      String senha = scanner.next();
+
 
 
 
