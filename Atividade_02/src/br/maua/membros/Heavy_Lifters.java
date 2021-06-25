b12902f1baed071487a16bb77eadf700f841333c
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 13:54:34 -0300

    Adicao dos atributos das mensagens regular e extra da classe Heavy_Lifters

--- Atividade_02/src/br/maua/membros/Heavy_Lifters.java
+++ Atividade_02/src/br/maua/membros/Heavy_Lifters.java
@@ -1,4 +1,6 @@
 package br.maua.membros;
 
 public class Heavy_Lifters extends Membro{
+    private String assinaturaRegular = "Podem contar conosco!";
+    private String assinaturaExtra = "N00b_qu3_n_Se_r3pita.bat";
 }
