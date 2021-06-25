04b614a0b32bbcbaaf60f8b91b44deb27513067d
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 18:33:15 -0300

    Implementacao dos metodos apresentar e postarMensagem no Mobile_Members

--- Atividade_02/src/br/maua/membros/Mobile_Members.java
+++ Atividade_02/src/br/maua/membros/Mobile_Members.java
@@ -1,6 +1,7 @@
 package br.maua.membros;
 
 import br.maua.enums.Funcao;
+import br.maua.enums.Horario;
 
 public class Mobile_Members extends Membro{
     String assinaturaRegular = "Happy Coding!";
@@ -10,5 +11,18 @@ public class Mobile_Members extends Membro{
 
     public Mobile_Members(String nickname, String senha, Funcao funcao) {
         super(nickname, senha, funcao);
+    }
+
+    @Override
+    public String apresentar() {
+        return "Ola eu sou o "+nickname+" da MAsK_S0c13ty, sou um dos Mobile Members.";
+    }
+
+    @Override
+    public String postarMensagem(Horario horario) {
+        if(horario == Horario.REGULAR){
+            return assinaturaRegular;
+        }
+        else{return assinaturaExtra;}
     }
 }
