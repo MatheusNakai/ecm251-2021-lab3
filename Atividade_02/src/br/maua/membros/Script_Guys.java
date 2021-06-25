f1e5795b5e3195190f648a2f511f85a2c5b5a818
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 13:57:14 -0300

    Implementacao do contrutor da classe Script_Guys com o super

--- Atividade_02/src/br/maua/membros/Script_Guys.java
+++ Atividade_02/src/br/maua/membros/Script_Guys.java
@@ -1,4 +1,12 @@
 package br.maua.membros;
 
+import br.maua.enums.Funcao;
+
 public class Script_Guys extends Membro{
+
+
+
+    public Script_Guys(String nickname, String senha, Funcao funcao) {
+        super(nickname, senha, funcao);
+    }
 }
