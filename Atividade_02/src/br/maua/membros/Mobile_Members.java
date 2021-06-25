5c27d74575781ace22fd65d9cd83d08ea8055582
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 16:52:27 -0300

    Adicao de atributos: assinaturaRegular assinaturaExtra
    Adicao do construtor com super.

--- Atividade_02/src/br/maua/membros/Mobile_Members.java
+++ Atividade_02/src/br/maua/membros/Mobile_Members.java
@@ -1,4 +1,14 @@
 package br.maua.membros;
 
+import br.maua.enums.Funcao;
+
 public class Mobile_Members extends Membro{
+    String assinaturaRegular = "Happy Coding!";
+    String assinaturaExtra = "MAsK_S0c13ty";
+
+
+
+    public Mobile_Members(String nickname, String senha, Funcao funcao) {
+        super(nickname, senha, funcao);
+    }
 }
