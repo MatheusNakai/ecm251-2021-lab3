571eaba165511cf6aa1971926d9ce8db5240ffb8
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 13:49:37 -0300

    Adicao dos 2 tipos de assinatura da classe Big_Brother

--- Atividade_02/src/br/maua/membros/Big_Brothers.java
+++ Atividade_02/src/br/maua/membros/Big_Brothers.java
@@ -1,4 +1,7 @@
 package br.maua.membros;
 
 public class Big_Brothers extends Membro{
+    private String assinaturaRegular = "Sempre ajudando as pessoas membros ou não S2!";
+    private String assinaturaExtra = "...";
+
 }
