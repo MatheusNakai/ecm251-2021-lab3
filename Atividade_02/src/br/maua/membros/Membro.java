bbb53107a9f91b8810af0fc3137b350c6309d4dc
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 17:26:19 -0300

    Implementacao do metodo que retorna em qual tipo de horario o membro esta

--- Atividade_02/src/br/maua/membros/Membro.java
+++ Atividade_02/src/br/maua/membros/Membro.java
@@ -1,6 +1,13 @@
 package br.maua.membros;
 
 import br.maua.enums.Funcao;
+import br.maua.enums.Horario;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public abstract class Membro {
     // Atributos compartilhados por todos os Membros
@@ -11,6 +18,7 @@ public abstract class Membro {
     protected Funcao funcao;
     protected String assinaturaRegular;
     protected String assinaturaExtra;
+
     // Controlador do id dos Membros
     private static int contador = 1;
 
@@ -29,5 +37,18 @@ public abstract class Membro {
 
         this.id=Membro.contador;
         Membro.contador++;                                                                                              //Adiciona mais um ao contador para cada Membro criado
+    }
+
+    public Horario getHorario() throws ParseException {
+        Calendar C = new GregorianCalendar();
+        //Armazena a hora do horario local
+        int hour = C.get( Calendar.HOUR_OF_DAY );
+        int minute = C.get( Calendar.MINUTE );
+
+        if( (hour >=8 && minute >= 0) && (hour<=17 && minute<=59) )
+            return Horario.REGULAR;
+        else{
+            return Horario.EXTRA;
+        }
     }
 }
