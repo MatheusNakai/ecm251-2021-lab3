67d435246647bc6a54bf4d64aa8dcd975066b747
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 13:56:26 -0300

    Implementacao do contrutor da classe Heavy_Lifters com o super

--- Atividade_02/src/br/maua/membros/Heavy_Lifters.java
+++ Atividade_02/src/br/maua/membros/Heavy_Lifters.java
@@ -1,6 +1,13 @@
 package br.maua.membros;
 
+import br.maua.enums.Funcao;
+
 public class Heavy_Lifters extends Membro{
     private String assinaturaRegular = "Podem contar conosco!";
     private String assinaturaExtra = "N00b_qu3_n_Se_r3pita.bat";
+
+
+    public Heavy_Lifters(String nickname, String senha, Funcao funcao) {
+        super(nickname, senha, funcao);
+    }
 }
