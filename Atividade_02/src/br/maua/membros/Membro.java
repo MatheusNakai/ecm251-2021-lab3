46966d3c2d7ce3e499b1080a215e21b438f5249f
MatheusNakai <MatheusNakai@users.noreply.github.com>
2021-06-25 13:34:55 -0300

    Adicao de Atributos: funcao contador

--- Atividade_02/src/br/maua/membros/Membro.java
+++ Atividade_02/src/br/maua/membros/Membro.java
@@ -1,8 +1,17 @@
 package br.maua.membros;
 
+import br.maua.enums.Funcao;
+
 public abstract class Membro {
+    // Atributos compartilhados por todos os Membros
+
     protected String nickname;
     protected String senha;
-    protected int id;
+    public static int id;
+    protected Funcao funcao;
+
+    // Controlador do id dos Membros
+    private static int contador = 1;
+
 
 }
